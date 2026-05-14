91f921ac81c70a3466e2effabff9d495814ecd94
unknown <you@example.com>
2026-05-14 13:13:06 -0300

    Preparation for json writing/reading

--- src/main/java/App/example/App.java
+++ src/main/java/App/example/App.java
@@ -2,15 +2,16 @@ package App.example;
 
 import Model.ResponseModel;
 import Server.Server;
-
+import com.google.gson.Gson;
 public class App
 {
     public static void main( String[] args )
     {
         System.out.println("Simulating servers");
+        System.out.println();
         for (int i = 1; i <= 10; i++){
             Server server = new Server();
-            ResponseModel response = server.runAndWrite();
+            server.runAndWrite("file_name.json");
         }
     }
 }
--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -6,6 +6,7 @@ import Model.Status;
 import Service.AuthServer.AuthServerService;
 import Service.DatabaseServer.DatabaseServerService;
 import Service.WebServer.WebServerService;
+import com.google.gson.Gson;
 
 public class Server {
     AuthServerService authServerService;
@@ -25,7 +26,7 @@ public class Server {
         responseModel.setStatus(Status.ERROR);
         responseModel.setPlace(place);
     }
-    public ResponseModel runAndWrite() {
+    public void runAndWrite(String fileName) {
         Status webstatus = this.webServerService.webServerSuccess();
         if (webstatus == Status.NOMINAL) {
             Status authStatus = this.authServerService.AuthSuccess();
@@ -42,18 +43,11 @@ public class Server {
         } else {
             fail("WEB");
         }
-        if (this.responseModel.getStatus() == Status.ERROR) {
-            System.out.printf("Request ID: %s\n",this.requestModel.getId());
-            System.out.println("Request TIME: " + this.requestModel.getTimestamp());
-            System.out.println(this.responseModel.getStatus().getMessage());
-            System.out.println("Error in : " + this.responseModel.getPlace());
-            System.out.println();
-        } else {
-            System.out.printf("Request ID: %s\n",this.requestModel.getId());
-            System.out.println("Request TIME: " + this.requestModel.getTimestamp());
-            System.out.println(this.responseModel.getStatus().getMessage());
-            System.out.println();
+        JsonObject jsonObject = new JsonObject(this.requestModel, this.responseModel);
+        try{
+            jsonObject.jsonWriting(jsonObject,fileName);
+        } catch(Exception e){
+            throw new RuntimeException();
         }
-        return responseModel;
     }
 }
