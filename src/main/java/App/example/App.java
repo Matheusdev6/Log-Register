aeebe3440f00735c93e38052ad5ad2677cc6784b
unknown <you@example.com>
2026-05-22 15:20:50 -0300

    Application menu, and advances in the DB connection and Updates functions.

--- src/main/java/App/example/App.java
+++ src/main/java/App/example/App.java
@@ -2,11 +2,40 @@ package App.example;
 
 import Model.DAO_Object;
 
+import java.util.Scanner;
+
 public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Simulating servers!");
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("\t>>>>>Server simulator<<<<<<");
+        System.out.println("\t\t\t>>Menu<<");
+        System.out.println("Digite o número da respectiva ação:");
+        System.out.println("\t[1]: Rodar servidores\n\t[2]: Exibir relatório\n\t[3] Sair");
+        System.out.print("Digite o número e pressione ENTER: ");
+        int escolha = sc.nextInt();
+        System.out.printf("Número escolhido: %d\n", escolha);
+        while(escolha != 3){
+            switch(escolha){
+                case 1:
+                    //função rodar servers
+                    System.out.println("Servidores rolando!");
+                    break;
+                case 2:
+                    //função relatório
+                    System.out.println("Relatório exbido.");
+                    break;
+                default:
+                    System.out.println("Digite uma opção válida - 1 a 3.");
+            }
+            System.out.println("Digite o número da respectiva ação:");
+            System.out.println("\t[1]: Rodar servidores\n\t[2]: Exibir relatório\n\t[3] Sair");
+            System.out.print("Digite o número e pressione ENTER: ");
+            escolha = sc.nextInt();
+            System.out.printf("Número escolhido: %d\n", escolha);
+        }*/
+
         for (int i = 1; i <= 5; i++){
             DAO_Object dao = new DAO_Object();
             dao.saveObject("S2");
--- src/main/java/Model/DAO_Object.java
+++ src/main/java/Model/DAO_Object.java
@@ -6,6 +6,7 @@ import Service.DBConnector;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DAO_Object {
     String id_request;
@@ -42,26 +43,32 @@ public class DAO_Object {
                     """,server);
         String command_2 = String.format("""
                       UPDATE %s.%s_Data
-                      SET\s
-                        %s_success = CASE WHEN %s.status = ? THEN %s_success + 1 ELSE %s_success END,
-                        %s_errors = CASE WHEN %s.status = ? THEN %s_errors + 1 ELSE %s_errors END
-                      WHERE rowid = \s
-                     \s""","report_DB",server,server,server,server,server,server,server,server,server);
+                      SET
+                        %s_success = CASE WHEN (SELECT status FROM %s WHERE id_request = ?) = ? THEN %s_success + 1 ELSE %s_success END,
+                        %s_errors = CASE WHEN (SELECT status FROM %s WHERE id_request = ?) = ? THEN %s_errors + 1 ELSE %s_errors END,
+                        %s_requests = (SELECT MAX(id) FROM %s)
+                      WHERE id = ?;
+                     ""","report_DB",server,server,server,server,server,server,server,server,server,server,server);
         Server serverObject = new Server();
         Connection con = DBConnector.getConnection();
         serverObject.run();
         DAO_Object saveObject = new DAO_Object(serverObject.getRequestModel(), serverObject.getResponseModel());
         try{
+            Statement attachStmt = con.createStatement();
+            attachStmt.execute(attachDatabases);
+            attachStmt.close();
             PreparedStatement stmt = con.prepareStatement(command_1);
-            stmt.execute(attachDatabases);
             stmt.setString(1, saveObject.getId_request());
             stmt.setString(2, saveObject.getStatus());
             stmt.setString(3, saveObject.getPlace());
             stmt.setString(4, saveObject.getTimestamp());
             stmt.execute();
             PreparedStatement stmt2 = con.prepareStatement(command_2);
-            stmt2.setString(1, "SUCCESS");
-            stmt2.setString(2, "ERROR");
+            stmt2.setString(1, saveObject.getId_request());
+            stmt2.setString(2, "SUCCESS");
+            stmt2.setString(3, saveObject.getId_request());
+            stmt2.setString(4, "ERROR");
+            stmt2.setInt(5,1);
             stmt2.execute();
             stmt.close();
         } catch (SQLException e) {
